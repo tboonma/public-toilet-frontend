6e04df1b2ad5e34cc97079aa3f5c2c231e59f13a
tboonma <tboonma@hotmail.com>
2022-02-12 11:50:58 +0700

    Insert more information in toilet card

--- src/components/ToiletCard.js
+++ src/components/ToiletCard.js
@@ -2,8 +2,16 @@ import react from 'react'
 
 const ToiletCard = (toilet) => {
   return (
-    <div class="toilet-card">
-      <h1>{toilet.title}</h1>
+    <div className="toilet-card">
+      <img
+        src="http://pngimg.com/uploads/toilet/toilet_PNG17753.png"
+        width="100px"
+        height="100px"
+        alt={toilet.title}
+      ></img>
+      <h1 className="toilet-card-title">{toilet.title}</h1>
+      <h2 className="toilet-card-time">{toilet.time}</h2>
+      <h2 className="toilet-card-elapsed">{toilet.elapsed}</h2>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,18 @@ import ToiletCard from '../components/ToiletCard.js'
 import '../components/ToiletCard.css'
 
 const Index = () => {
-  return <ToiletCard title="Test Toilet 1" />
+  return (
+    <div>
+      <div class="page-title">
+        <h1>Toilet Management</h1>
+      </div>
+      <div class="toilets-section">
+        <ToiletCard title="Test Toilet 1" />
+        <ToiletCard title="Test Toilet 1" />
+        <ToiletCard title="Test Toilet 1" />
+      </div>
+    </div>
+  )
 }
 
 export default Index
