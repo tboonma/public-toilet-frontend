39f939c2d5f7b23dbb575f27a4c85d08405127e7
tboonma <tboonma@hotmail.com>
2022-02-12 14:36:26 +0700

    Finish improve UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,39 @@
-import React from "react"
-import ToiletCard from "../components/ToiletCard.js"
-import "../components/ToiletCard.css"
-import Banner from "../components/Banner.js"
+import React from 'react'
+import ToiletCard from '../components/ToiletCard.js'
+import '../components/ToiletCard.css'
+import Banner from '../components/Banner.js'
+import Background from '../background.jpg'
 
 const Index = () => {
   return (
-    <div Index>
-      <Banner />
-      <div className="allcard">
-        <ToiletCard
-          title="Test Toilet 1"
-          time="12.59"
-          timer="15.00"
-          status="open"
-        />
-        <ToiletCard
-          title="Test Toilet 2"
-          time="13.59"
-          timer="10.00"
-          status="closed"
-        />
-        <ToiletCard
-          title="Test Toilet 3"
-          time="1.59"
-          timer="11.00"
-          status="open"
-        />
+    <div
+      className="screen"
+      style={{
+        backgroundImage: `url(${Background})`,
+      }}
+    >
+      <div className="window-padding">
+        <Banner />
+        <div className="allcard">
+          <ToiletCard
+            title="Test Toilet 1"
+            time="12.59"
+            timer="15.00"
+            status="open"
+          />
+          <ToiletCard
+            title="Test Toilet 2"
+            time="13.59"
+            timer="10.00"
+            status="closed"
+          />
+          <ToiletCard
+            title="Test Toilet 3"
+            time="1.59"
+            timer="11.00"
+            status="open"
+          />
+        </div>
       </div>
     </div>
   )
